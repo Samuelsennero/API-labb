ee5112be5c78de0b5014bf4184ac8e8f2bfe3066
SamuelSennero <samuel.sennero@gmail.com>
2020-05-11 16:59:28 +0200

    miss

--- weather.py
+++ weather.py
@@ -8,7 +8,7 @@ link_key = input_key()
 
 url = (link_key.format(city))
 
-res = requests.get(url) #413e26530db141f29a0605568691c961
+res = requests.get(url) 
 output = res.json()
 
 values = [(output['data'][0]['app_temp']), (output['data'][0]['sunrise']), (output['data'][0]['sunset']) ]
