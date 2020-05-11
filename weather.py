ff74e6a493f8c9225ab0550a90c9e88afb92e892
SamuelSennero <samuel.sennero@gmail.com>
2020-05-11 15:08:00 +0200

    hide key

--- weather.py
+++ weather.py
@@ -1,11 +1,14 @@
 import requests
 import json
+from config import input_key
 
 city = input('Enter a city: ')
 
-url = 'https://api.weatherbit.io/v2.0/current?city={}&key=413e26530db141f29a0605568691c961'.format(city)
+x = input_key()
 
-res = requests.get(url)
+url = (x.format(city))
+
+res = requests.get(url) #413e26530db141f29a0605568691c961
 output = res.json()
 
 values = [(output['data'][0]['app_temp']), (output['data'][0]['sunrise']), (output['data'][0]['sunset']) ]
